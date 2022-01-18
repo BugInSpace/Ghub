a214f8047d8fe638c6cc595ad0000110f85dba0f
BugInSpace <97572924+BugInSpace@users.noreply.github.com>
2022-01-18 14:48:08 +0300

    Add files via upload

---
+++ Learning/JavaScript/JS.js
@@ -0,0 +1,21 @@
+function calculateAge(year) {
+    return 2022 - year
+}
+
+function logInfoAbout(name, year) {
+    return age = calculateAge(year)
+}
+if (age > 0) {
+    console.log('Человек по имени ' + name + 'сейчас имеет возраст' + age);
+}
+else {
+    console.log('Такое невозможно');
+}
+logInfoAbout('Азамат', 22)
+
+if (a < b) {
+    console.log('Thats incredible');
+} else { console.log('that usually'); }
+
+
+
